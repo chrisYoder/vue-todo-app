6302a68d43fe2b20d366d8edc0d430275c171e4c
Chris <christophermyoder@gmail.com>
2019-09-19 15:26:41 -0500

    eod commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1 class="center">Vue.js Todo App</h1>
+    <div class="ui divider"></div>
+
+    <todo-list v-bind:todos="todos"/>
+    <create-todo v-on:add-todo="addTodo"/>
+  </div>
 </template>
 
 <script>
 import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo";
 
 export default {
   name: "App",
-  components: {}
+  components: {
+    TodoList,
+    CreateTodo
+  },
+
+  data() {
+    return {
+      todos: [
+        {
+          title: "Todo A",
+          project: "Project A",
+          done: false
+        },
+        {
+          title: "Todo B",
+          project: "Project B",
+          done: true
+        },
+        {
+          title: "Todo C",
+          project: "Project C",
+          done: false
+        },
+        {
+          title: "Todo D",
+          project: "Project D",
+          done: false
+        }
+      ]
+    };
+  },
+
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false
+      });
+    }
+  }
 };
 </script>
 
@@ -16,8 +62,15 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1 {
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+
+.divider {
+  margin-top: 0;
+  padding-top: 0;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="ui centered card">
+    <div class="content" v-show="!isEditing">
+      <div class="header">{{ todo.title }}</div>
+      <div class="meta">{{ todo.project }}</div>
+      <div class="extra content">
+        <span class="right floated edit icon" @click="showForm">
+          <i class="edit icon"></i>
+        </span>
+        <span class="right floated trash icon" @click="deleteTodo(todo)">
+          <i class="trash alternate icon"></i>
+        </span>
+      </div>
+    </div>
+    <!-- end of !isEditing -->
+    <div class="content" v-show="isEditing">
+      <div class="ui form">
+        <div class="field">
+          <label>Title</label>
+          <input type="text" name="title" v-model="todo.title">
+        </div>
+        <div class="field">
+          <label>Project</label>
+          <input type="text" name="project" v-model="todo.project">
+        </div>
+        <div class="ui two button attached buttons">
+          <button class="ui basic blue button" @click="hideForm">Close X</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui bottom attached green basic button" v-show="todo.done">Completed</div>
+    <div class="ui bottom attached red basic button" v-show="!todo.done">Complete</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todo"],
+  data() {
+    return {
+      isEditing: false
+    };
+  },
+  methods: {
+    hideForm() {
+      this.isEditing = false;
+    },
+    showForm() {
+      this.isEditing = true;
+    }, 
+    deleteTodo(todo) {
+      this.$emit('delete-todo', todo);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
